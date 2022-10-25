84e8cb4e73e1ea3126df2e4460ca3d7cd8c52b4c
Josiane Cristina Henrique dos Santos <jacphs1326@outlook.com>
2022-10-25 17:30:15 -0300

    Desafio de Projeto Hospedagem Concluído!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,5 +1,5 @@
 namespace DesafioProjetoHospedagem.Models
-{
+{    
     public class Reserva
     {
         public List<Pessoa> Hospedes { get; set; }
@@ -16,15 +16,16 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *Implementado!!!*
+            if (Suite.Capacidade >= 2)
             {
                 Hospedes = hospedes;
             }
             else
             {
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte");
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *Implementado!!!*
             }
         }
 
@@ -36,22 +37,27 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // *Implementado!!!*
+            int quantidadeHospedes = Hospedes.Count();
+            return quantidadeHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // *Implementado!!!*
+            decimal quantidadeDias = DiasReservados;
+            decimal valorDiaria = Suite.ValorDiaria;
+            decimal valorFinal = Convert.ToDecimal(quantidadeDias) * valorDiaria;
+            decimal valor = Convert.ToDecimal(valorFinal);
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *Implementado!!!*
+            if (DiasReservados >= 5)
             {
-                valor = 0;
+                double desconto = 0.10 * Convert.ToDouble(valor);
+                valor = valor - Convert.ToDecimal(desconto);
             }
 
             return valor;
